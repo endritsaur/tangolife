516ed98727f4c34eb2a72a60ffd768e5f43327b9
Endrit <endritsaur@gmail.com>
2017-12-18 20:07:42 -0600

    created box class, added css and minor fixes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ class Main extends React.Component {
         this.cols = 50;
 
         this.state = {
+            iteration: 0,
             gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
         }
     }
@@ -18,10 +19,12 @@ class Main extends React.Component {
         return(
             <div>
                 <h1>Tango LIFE</h1>
+                <h4>Iterations: {this.state.iteration}</h4>
                 <Grid
                     gridFull = {this.state.gridFull}
                     rows = {this.rows}
                     cols = {this.cols}
+                    selectBox = {this.selectBox}
                 />
             </div>
         );
@@ -30,11 +33,40 @@ class Main extends React.Component {
 
 class Grid extends React.Component {
     render() {
-        const width = this.props.cols;
-        var rowsArray = [];
+        const width = this.props.cols * 14;
+        var rowsArray = []
+        var boxClass = "";
+
+        for(var i = 0; i < this.props.rows; i++) {
+            for(var j = 0; j < this.props.cols; j++) {
+                let boxId = i + "_" + j;
+
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArray.push(
+                    <Box 
+                        boxClass = {boxClass}
+                        key = {boxId}
+                        boxId = {boxId}
+                        row = {i}
+                        col = {j}
+                        selectBox = {this.props.selectBox}
+                    />
+                )
+            }
+        }
 
         return(
             <div className="">Content</div>
+        );
+    }
+}
+
+class Box extends React.Component {
+    render() {
+        return(
+            <div
+                className = {this.props.boxClass}
+            />
         );
     }
 }
