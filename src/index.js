63c92a0683d07f7a345fedf14948328a43ed4481
Endrit <endritsaur@gmail.com>
2017-12-18 19:46:56 -0600

    created main and grid classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+
+        this.state = {
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Tango LIFE</h1>
+                <Grid
+                    gridFull = {this.state.gridFull}
+                    rows = {this.rows}
+                    cols = {this.cols}
+                />
+            </div>
+        );
+    }
+}
+
+class Grid extends React.Component {
+    render() {
+        const width = this.props.cols;
+        var rowsArray = [];
+
+        return(
+            <div className="">Content</div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
